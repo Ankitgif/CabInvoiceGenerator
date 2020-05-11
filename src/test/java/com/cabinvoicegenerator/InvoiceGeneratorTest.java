4f0a3e43d225b9059134d51ed61d456ebc867d00
Ankitgif <ankitsinha2303@gmail.com>
2020-05-12 01:10:03 +0530

    [Ankit] Refactored , declared setUp method

--- src/test/java/com/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -1,12 +1,18 @@
 package com.cabinvoicegenerator;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceGeneratorTest {
+    InvoiceGenerator invoiceGenerator = null;
+
+    @Before
+    public void setUp() throws Exception{
+        invoiceGenerator = new InvoiceGenerator();
+    }
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance,time);
@@ -14,7 +20,6 @@ public class InvoiceGeneratorTest {
     }
     @Test
     public void givenLessDistanceOrTime_ShouldReturnMinFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance,time);
@@ -22,7 +27,6 @@ public class InvoiceGeneratorTest {
     }
     @Test
     public void givenMultipleRides_ShouldReturnTotalFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         Ride[] ride = {new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
