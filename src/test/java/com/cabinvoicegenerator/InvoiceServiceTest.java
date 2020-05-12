5ddc07c41dcd16a6cbebef7aa31c883ce9296aea
Ankitgif <ankitsinha2303@gmail.com>
2020-05-12 22:51:21 +0530

    [Ankit] Added Test Case for givenUserIdAndRides_ShouldReturnInvoiceSummary

---
+++ src/test/java/com/cabinvoicegenerator/InvoiceServiceTest.java
@@ -0,0 +1,49 @@
+package com.cabinvoicegenerator;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+    InvoiceService invoiceService = null;
+
+    @Before
+    public void setUp() throws Exception{
+        invoiceService = new InvoiceService();
+    }
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare(){
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceService.calculateFare(distance,time);
+        Assert.assertEquals(25,fare,0.0);
+    }
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare(){
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceService.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0.0) ;
+    }
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary(){
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        InvoiceSummary summary = invoiceService.calculateFare(rides);
+        InvoiceSummary expectedinvoiceSummary = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedinvoiceSummary,summary);
+    }
+    @Test
+    public void givenUserIdAndRides_ShouldReturnInvoiceSummary(){
+        String userId = "abc@.com";
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        invoiceService.addRides(userId,rides);
+        InvoiceSummary summary = invoiceService.getInvoiceSummary(userId);
+        InvoiceSummary expectedinvoiceSummary = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedinvoiceSummary,summary);
+
+    }
+}
