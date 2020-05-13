7521259aa2c48b193e8f8946fb5f4a5d1293d759
Ankitgif <ankitsinha2303@gmail.com>
2020-05-13 16:28:04 +0530

    [Ankit] Added rideType in constructor

--- src/main/java/com/cabinvoicegenerator/Ride.java
+++ src/main/java/com/cabinvoicegenerator/Ride.java
@@ -2,12 +2,16 @@ package com.cabinvoicegenerator;
 
 import java.util.ArrayList;
 
+
 public class Ride extends ArrayList<Ride> {
     public final double distance;
     public final int time;
+    public String rideType;
 
-    public Ride(double distance, int time) {
+    public Ride(double distance, int time, String rideType) {
         this.distance = distance;
         this.time = time;
+        this.rideType = rideType;
+
     }
 }
