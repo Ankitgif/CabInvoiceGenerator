86f69b87ff86d41e55243a142ac6e71996dc37d2
Ankitgif <ankitsinha2303@gmail.com>
2020-05-13 20:31:49 +0530

    [Ankit] Refactored, removed the enum operation

--- src/main/java/com/cabinvoicegenerator/InvoiceService.java
+++ src/main/java/com/cabinvoicegenerator/InvoiceService.java
@@ -2,36 +2,23 @@ package com.cabinvoicegenerator;
 
 public class InvoiceService {
 
-
-    private static final double MINIMUM_COST_PER_KILOMETER = 10;
-    private static final int COST_PER_TIME = 1;
-    private static final double MINIMUM_FARE = 5;
-    private static final double MINIMUM_COST_PER_KILOMETER_PREMIUM = 15;
-    private static final int COST_PER_TIME_PRIMIUM = 2;
-    private static final double MINIMUM_FARE_PREMIUM = 20;
-    private  RideRepository rideRepository;
+    private RideRepository rideRepository;
 
     public InvoiceService() {
         this.rideRepository = new RideRepository();
     }
 
-    public double calculateFare(double distance, int time, Ride.RideType type) {
-        if (type.equals(Ride.RideType.normal)) {
-            double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-            return Math.max(totalFare, MINIMUM_FARE);
-        } if (type.equals(Ride.RideType.premium)){
-            double totalFare = distance * MINIMUM_COST_PER_KILOMETER_PREMIUM + time * COST_PER_TIME_PRIMIUM;
-            return Math.max(totalFare, MINIMUM_FARE_PREMIUM);
-        }
-        return 0;
+    public double calculateFare(double distance, int time, CabRide type) {
+        Ride ride = new Ride(distance, time, type);
+        return type.calculateFare(ride);
     }
 
     public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0;
-        for (Ride ride:rides) {
-            totalFare += this.calculateFare(ride.distance,ride.time,ride.rideType);
+        for (Ride ride : rides) {
+            totalFare += ride.cabRide.calculateFare(ride);
         }
-        return new InvoiceSummary(rides.length,totalFare);
+        return new InvoiceSummary(rides.length, totalFare);
     }
 
     public void addRides(String userId, Ride[] rides) throws CabInvoiceException {
