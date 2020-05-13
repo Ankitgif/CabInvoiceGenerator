cc1f44055fb865221ed55e70bddad492a48d2e5c
Ankitgif <ankitsinha2303@gmail.com>
2020-05-13 11:47:22 +0530

    [Ankit] Added list of multiple rides

--- src/main/java/com/cabinvoicegenerator/RideRepository.java
+++ src/main/java/com/cabinvoicegenerator/RideRepository.java
@@ -1,9 +1,6 @@
 package com.cabinvoicegenerator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class RideRepository {
     Map<String, ArrayList<Ride>> userRides = null;
@@ -14,7 +11,13 @@ public class RideRepository {
     }
 
     public void addRides(String userId, Ride[] rides) {
+        boolean checkRide = userRides.containsKey(userId);
+        ArrayList<Ride> list = new ArrayList<Ride>() ;
+        if(checkRide == false){
+            list.addAll(Arrays.asList(rides));
             this.userRides.put(userId, new ArrayList<>(Arrays.asList(rides)));
+        }
+        else for (Ride ride : rides) userRides.get(userId).add(ride);
     }
 
     public Ride[] getRides(String userId) {
