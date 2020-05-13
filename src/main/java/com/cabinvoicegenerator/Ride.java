5d58b2cd5e5c3a1ab0e018b41cebfcd24eca25ee
Ankitgif <ankitsinha2303@gmail.com>
2020-05-13 18:52:33 +0530

    [Ankit] Added enum for normal and premium ride

--- src/main/java/com/cabinvoicegenerator/Ride.java
+++ src/main/java/com/cabinvoicegenerator/Ride.java
@@ -3,12 +3,17 @@ package com.cabinvoicegenerator;
 import java.util.ArrayList;
 
 
+
 public class Ride extends ArrayList<Ride> {
+
     public final double distance;
     public final int time;
-    public String rideType;
+    public final RideType rideType;
 
-    public Ride(double distance, int time, String rideType) {
+    public enum RideType{
+        normal,premium
+    }
+    public Ride(double distance, int time, RideType rideType) {
         this.distance = distance;
         this.time = time;
         this.rideType = rideType;
